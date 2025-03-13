16998dde3ccd1c815031bf45c89d7db51547ee31
eightypoint1 <EzekielAaronMarmora@proton.me>
2025-03-13 10:54:22 +0700

    Add komen

--- PRAKTIKUM PEMLAN/Tugas 3/praktic.geometry/mains/Main.java
+++ PRAKTIKUM PEMLAN/Tugas 3/praktic.geometry/mains/Main.java
@@ -1,32 +1,47 @@
 package praktic.geometry.mains;
 
+// Import Class Circle untuk mendeklarasi Objek Circle
 import praktic.geometry.shapes.Circle;
+// Import Scanner untuk input pengguna
 import java.util.Scanner;
 
 public class Main{
 
+    // String Kode ANSI untuk memberi warna pada output, static agar dapat digunakan tanpa instansiasi class
     public static String WHITE = "\u001B[0m";
     public static String GREEN = "\u001B[32m";
     public static String YELLOW = "\u001B[33m";
     public static String BLUE = "\u001B[94m";
 
+    // Method untuk print Garis, static agar dapat digunakan tanpai instansiasi class
     public static void printGaris(){
         System.out.println(WHITE + "====================");
     }
     public static void main(String[] args) {
+        // Instansiasi objek lingkaran
         Circle lingkaran = new Circle();
+
+        // Instansiasi Objek scanner
         Scanner scan = new Scanner(System.in);
+
+        // Print Informasi Mahasiswa
         printGaris();
         System.out.println("Nama : Ezekiel Aaron Marmora");
         System.out.println("NIM  : 245150701111017");
         printGaris();
+
         System.out.println("Circle");
         printGaris();
+
+        // Input ID objek lingkaran
         System.out.print("Isikan ID : "); 
         lingkaran.setId(scan.nextInt());
+
+        // Input Radius objek lingkaran
         System.out.print("Isikan Radius : ");
         lingkaran.setRadius(scan.nextDouble());
 
+        // Output Deskripsi objek lingkaran
         lingkaran.printDeskripsi();
     }
 }
--- PRAKTIKUM PEMLAN/Tugas 3/praktic.geometry/shapes/Circle.java
+++ PRAKTIKUM PEMLAN/Tugas 3/praktic.geometry/shapes/Circle.java
@@ -1,31 +1,39 @@
 package praktic.geometry.shapes;
 
+// Import class Main untuk warna dan method printGaris
 import praktic.geometry.mains.Main;
 
 public class Circle{
+    // Data Field
     private int id;
     private double radius;
     private final int pembilang = 22;
     private final int penyebut = 7;
 
+    // No arg constructor
     public Circle() {}
 
+    // ID Setter
     public void setId(int id){
         this.id = id;
     }
 
+    // Radius setter
     public void setRadius(double radius){
         this.radius = radius;
     }
 
+    // return nilai keliling dari radius input
     public double getKeliling(){
-        return 2*(Math.PI)*radius;
+        return (double) pembilang/penyebut*radius*2;
     }
 
+    // return nilai luas dari radius input pengguna
     public double getLuas() {
-        return radius*radius*(Math.PI);    
+        return (double) pembilang/penyebut*radius*radius;    
     }
 
+    // Print deskripsi dari object Circle: ID, Radius, Keliling, Luas
     public void printDeskripsi() {
         Main.printGaris();
         System.out.println(Main.WHITE + "ID       : " + id);
